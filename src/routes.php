5f911d46ebcce2f739ec969de520da1df25372bb
indes <wanglicsly@gmail.com>
2018-11-25 10:57:53 +0800

    feat(skeleton): skeleton and basic function

--- src/routes.php
+++ src/routes.php
@@ -4,23 +4,14 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use Longman\TelegramBot\Telegram;
 
-// Routes
-//$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-//    // Sample log message
-//    $this->logger->info("Slim-Skeleton '/' route");
-//
-//    // Render index view
-//    return $this->renderer->render($response, 'index.phtml', $args);
-//});
-
 
 $app->get("/", function (Request $request, Response $response, array $args) {
-    $response->getBody()->write(":)");
+    $response->getBody()->write("<h1>:)</h1>");
     return $response;
 });
 
 
-$app->get("/send", function (Request $request, Response $response, array $args) {
+$app->get("/ping", function (Request $request, Response $response, array $args) {
 
     $telegram = new Telegram(getenv('BOT_TOKEN'));
     if (getenv('PROXY')) {
@@ -32,10 +23,29 @@ $app->get("/send", function (Request $request, Response $response, array $args) 
         );
     }
 
+    $result = Longman\TelegramBot\Request::sendMessage([
+        'chat_id' => (int)getenv('OWNER_ID'),
+        'text' => "ping"
+    ]);
+    $response->getBody()->write("ping");
+    return $response;
+});
+
+$app->get("/api/send", function (Request $request, Response $response, array $args) {
+
+    $telegram = new Telegram(getenv('BOT_TOKEN'));
+    if (getenv('PROXY')) {
+        Longman\TelegramBot\Request::setClient(
+            new \GuzzleHttp\Client([
+                'base_uri' => 'https://api.telegram.org',
+                'proxy' => getenv('PROXY')
+            ])
+        );
+    }
 
     $result = Longman\TelegramBot\Request::sendMessage([
         'chat_id' => (int)getenv('OWNER_ID'),
-        'text' => (int)getenv('OWNER_ID')
+        'text' => $request->getQueryParam("msg")
     ]);
     $response->getBody()->write("ok");
     return $response;
