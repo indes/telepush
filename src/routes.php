92caac73f5fc67e46c33bd71801dd41855feb90f
indes <wl@live.it>
2018-12-07 16:50:29 +0800

    feat(send): Photo send

--- src/routes.php
+++ src/routes.php
@@ -32,7 +32,7 @@ $app->get("/ping", function (Request $request, Response $response, array $args) 
 });
 
 
-$app->map(['GET', 'POST'], "/api/send", function (Request $request, Response $response, array $args) {
+$app->map(['GET', 'POST'], "/api/msg", function (Request $request, Response $response, array $args) {
 
     $telegram = new Telegram(getenv('BOT_TOKEN'));
     if (getenv('PROXY')) {
@@ -51,6 +51,36 @@ $app->map(['GET', 'POST'], "/api/send", function (Request $request, Response $re
     $result = Longman\TelegramBot\Request::sendMessage([
         'chat_id' => (int)getenv('OWNER_ID'),
         'text' => $text
+    ]);
+
+
+    if ($result->isOk()) {
+        return $response->withJson(["ok" => true]);
+    } else {
+        return $response->withJson($result);
+    }
+
+});
+
+$app->map(['GET', 'POST'], "/api/photo", function (Request $request, Response $response, array $args) {
+
+    $telegram = new Telegram(getenv('BOT_TOKEN'));
+    if (getenv('PROXY')) {
+        Longman\TelegramBot\Request::setClient(
+            new \GuzzleHttp\Client([
+                'base_uri' => 'https://api.telegram.org',
+                'proxy' => getenv('PROXY')
+            ])
+        );
+    }
+
+    $parsedBody = $request->getParsedBody();
+
+    $url = $parsedBody["url"] ? $parsedBody["url"] : $request->getQueryParam("url");
+
+    $result = Longman\TelegramBot\Request::sendPhoto([
+        'chat_id' => (int)getenv('OWNER_ID'),
+        'photo'   => $url
     ]);
 
 
